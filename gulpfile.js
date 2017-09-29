9a3769a830b5e1ae65c9bce3129ab4c2faf9d2bc
armando.sierralta <armando.sierralta@truextend.com>
2017-09-29 16:22:20 -0400

    remove comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,12 @@ var del = require('del');
 var webpack = require('webpack');
 var webpack_stream = require('webpack-stream');
 var webpack_config = require('./webpack.config');
-//var embedTemplates = require('gulp-angular-embed-templates');
 
 gulp.task('clean', function () {
     del([
         './dist'
     ]);
 });
-
-//gulp.task('embed', function () {
-//    gulp.src('./app/**/*.js')
-//        .pipe(embedTemplates());
-//});
 
 gulp.task('webpack', ['clean'], function () {
     return webpack_stream(webpack_config)
